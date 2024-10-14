f52f2ab55d68790634a45a0d2d8ceae93154fb2a
jhansarmiento <jhancsn1@gmail.com>
2024-10-13 19:38:21 -0500

    Aplica Type Guards a la respuesta de la API

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -1,5 +1,17 @@
 import axios from 'axios'
-import { SearchType } from '../types'
+import { SearchType, Weather } from '../types'
+
+// Type Guard
+function isWeatherResponse(weather : unknown) : weather is Weather {
+    return (
+        Boolean(weather) &&
+        typeof weather === 'object' &&
+        typeof (weather as Weather).name === 'string' &&
+        typeof (weather as Weather).main.temp === 'number' &&
+        typeof (weather as Weather).main.temp_max === 'number' &&
+        typeof (weather as Weather).main.temp_min === 'number'
+    )
+}
 
 export default function useWeather () {
 
@@ -11,7 +23,14 @@ export default function useWeather () {
             const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`
 
             const {data} = await axios(geoUrl)
-            console.log(data)
+            const lat = data[0].lat
+            const lon = data[0].lon
+            
+            const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`
+            const { data : weatherResult} = await axios(weatherUrl)
+            // Type Guard
+            const result = isWeatherResponse(weatherResult)
+            console.log(result)
 
         } catch (error) {
             console.log(error)
--- src/types/index.ts
+++ src/types/index.ts
@@ -7,3 +7,12 @@ export type Country = {
     code: string
     name: string
 }
+
+export type Weather = {
+    name : string
+    main : {
+        temp : 296.03
+        temp_max : 296.03
+        temp_min : 296.03
+    }
+}
