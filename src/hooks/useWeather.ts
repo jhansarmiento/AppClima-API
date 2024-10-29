d43a7781a9fdcebfcd7796d103feabda3754e523
jhansarmiento <jhancsn1@gmail.com>
2024-10-28 21:30:28 -0500

    Aplica Valibot para tipar la respuesta de la API

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -1,19 +1,19 @@
 import axios from 'axios'
-import { z } from 'zod'
+import { object, string, number, InferOutput, parse } from 'valibot'
 import { SearchType } from '../types'
 
-// ZOD
-// Creamos primero el schema que será una especia de Type
-const Weather = z.object({
-    name: z.string(),
-    main: z.object({
-        temp: z.number(),
-        temp_max: z.number(),
-        temp_min: z.number()
+// Valibot
+// Al igual que en ZOD creamos primero el schema que será una especia de Type
+const WeatherSchema = object({
+    name: string(),
+    main: object({
+        temp: number(),
+        temp_max: number(),
+        temp_min: number()
     })
 })
 
-type Weather = z.infer<typeof Weather>
+type Weather = InferOutput<typeof WeatherSchema>
 
 export default function useWeather () {
 
@@ -30,13 +30,13 @@ export default function useWeather () {
 
             const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`
 
-            // ZOD Guards
+            // Valibot Guards
             const { data : weatherResult} = await axios(weatherUrl)
-            const result = Weather.safeParse(weatherResult)
+            const result = parse(WeatherSchema, weatherResult)
 
-            if(result.success) {
-                console.log(result.data.name)
-                console.log(result.data.main.temp)
+            if(result) {
+                console.log(result.name)
+                console.log(result.main.temp)
             }
             
 
